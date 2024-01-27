723d7b368f86e9ee91427bb701bf31fea442ae33
xvqh <138306559+sewiiiii@users.noreply.github.com>
2024-01-27 14:00:59 +0100

    Update fun.js

--- commands/Helps/fun.js
+++ commands/Helps/fun.js
@@ -1,34 +1,15 @@
 const Discord = require("discord.js-selfbot-v13");
-const { language } = require("../../fonctions");
 
 module.exports = {
   name: "fun",
   description: "Menu fun",
   run: async (client, message, db, args, prefix) => {
     try{
-
-        message.edit(await language(client, `
+        message.edit(`
 ⛧ __**Xyla - Fun**__ ⛧
-\`${prefix}say <@user> [message]\` ➜ **Dire une chose sous l'identité d'autrui (webhook)**
-\`${prefix}thot <@user>\` ➜ **Note le pourçentage de saloperie d'autrui**
-\`${prefix}love <@user>\` ➜ **Message animé**
-\`${prefix}nitro\` ➜ **Drop de faux nitro**
-\`${prefix}noadd [delete]\` ➜ **Empêche les gens d'ajouter des personnes au groupe**
-\`${prefix}noleave [delete]\` ➜ **Ré-ajoute les gens qui quittent le groupe (vos amis seulement)**
 \`${prefix}add\` ➜ **Envoi ton lien pour t'ajouter**
-\`${prefix}robuser <@user>\` ➜ **Vole le profil d'un membre**`,
-        
-        `⛧ __**Xyla - Fun**__ ⛧
-\`${prefix}say <@user> [message]\` ➜ **Saying something under someone else's identity (webhook)**
-\`${prefix}thot <@user>\` ➜ **Note the percentage of other people's filth**
-\`${prefix}love <@user>\` ➜ **Animated message**
-\`${prefix}nitro\` ➜ **Send fakes nitros**
-\`${prefix}noadd [delete]\` ➜ **Prevents people from adding people to the group**
-\`${prefix}noleave [delete]\` ➜ **Re-add people who leave the group (your friends only)**
-\`${prefix}add\` ➜ **Send your link to add yourself**
-\`${prefix}robuser <@user>\` ➜ **Steal a member's profile**`
+\`${prefix}robuser <@user>\` ➜ **Vole le profil d'un membre**`
         ))
-
       }
         catch(e){}
     }

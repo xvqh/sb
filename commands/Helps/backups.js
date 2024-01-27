45e79261194d56360a076e1256f306bbe3af289f
xvqh <138306559+sewiiiii@users.noreply.github.com>
2024-01-27 14:00:10 +0100

    Update backups.js

--- commands/Helps/backups.js
+++ commands/Helps/backups.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js-selfbot-v13");
-const {  language } = require("../../fonctions")
 
 module.exports = {
   name: "backups",
@@ -9,12 +8,7 @@ module.exports = {
 \`${prefix}backup create\` ➜ **crée une backup**
 \`${prefix}backup load\` ➜ **charge une backup dans un serveur**
 \`${prefix}backup delete\` ➜ **supprimer une backup (en dev)**
-\`${prefix}backup list\` ➜ **la list de t'es backup (en dev)**`,
-    `⛧ __**Xyla - Backup**__ ⛧
-    \`${prefix}backup create\` ➜ **create a backup**
-    \`${prefix}backup load\` ➜ **upload a save to a server**
-    \`${prefix}backup delete\` ➜ **delete a backup (in dev)**
-    \`${prefix}backup list\` ➜ **the list of your backup (en dev)**`
+\`${prefix}backup list\` ➜ **la list de t'es backup (en dev)**`
     ))
   }
 }

936d5f578a752f15b100f88bc57d6097e170f7da
xvqh <138306559+sewiiiii@users.noreply.github.com>
2024-01-27 14:28:31 +0100

    Update backups.js

--- commands/Helps/backups.js
+++ commands/Helps/backups.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js-selfbot-v13");
-
+const config = require("../../config")
+const prefix = config.prefix
 module.exports = {
   name: "backups",
   description: "backup Mode",
-  run: async (client, message, args, db, prefix) => {
+  run: async (client, message, args) => {
       message.edit(`⛧ __**Xyla - Backup**__ ⛧
 \`${prefix}backup create\` ➜ **crée une backup**
 \`${prefix}backup load\` ➜ **charge une backup dans un serveur**

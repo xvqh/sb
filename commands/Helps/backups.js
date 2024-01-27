6a5b622070115812b83a6c4a850e466b33782985
xvqh <138306559+sewiiiii@users.noreply.github.com>
2024-01-27 14:01:31 +0100

    Update backups.js

--- commands/Helps/backups.js
+++ commands/Helps/backups.js
@@ -9,6 +9,6 @@ module.exports = {
 \`${prefix}backup load\` ➜ **charge une backup dans un serveur**
 \`${prefix}backup delete\` ➜ **supprimer une backup (en dev)**
 \`${prefix}backup list\` ➜ **la list de t'es backup (en dev)**`
-    ))
+    )
   }
 }

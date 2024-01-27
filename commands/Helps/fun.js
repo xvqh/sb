4578e684d519bba8b407de9370b00d62a4f6d90a
xvqh <138306559+sewiiiii@users.noreply.github.com>
2024-01-27 14:01:42 +0100

    Update fun.js

--- commands/Helps/fun.js
+++ commands/Helps/fun.js
@@ -9,7 +9,7 @@ module.exports = {
 ⛧ __**Xyla - Fun**__ ⛧
 \`${prefix}add\` ➜ **Envoi ton lien pour t'ajouter**
 \`${prefix}robuser <@user>\` ➜ **Vole le profil d'un membre**`
-        ))
+        )
       }
         catch(e){}
     }
